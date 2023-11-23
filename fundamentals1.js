51036ca12298732348646b1d1ed2b3d65f76598e
Brad Meyer <wbradmeyer@gmail.com>
2023-11-22 21:59:27 -0500

    final exercise

--- fundamentals1.js
+++ fundamentals1.js
@@ -166,3 +166,26 @@ const countDownWithFor = (lowNum, highNum, mult) => {
 
 countDownWithFor(2, 9, 3)
 countDownWithFor(3, 17, 4)
+
+
+// The Final Countdown
+/* This is based on “Flexible Countdown”. The parameter names are not as helpful, 
+but the problem is essentially identical; don’t be thrown off! Given 4 parameters 
+(param1,param2,param3,param4), print the multiples of param1, starting at param2 
+and extending to param3. One exception: if a multiple is equal to param4, then skip 
+(don’t print) it. Do this using a WHILE. Given (3,5,17,9), print 6,12,15 (which are 
+all of the multiples of 3 between 5 and 17, and excluding the value 9). */
+
+const finalCountDown = (param1, param2, param3, param4) => {
+    let count = param2
+    while(count <= param3)
+        if(count == param4 || count % param1){
+            count ++
+        } else {
+            console.log(count)
+            count += param1
+        }
+}
+
+finalCountDown(3, 5, 17, 9)
+finalCountDown(4, 2, 29, 20)
