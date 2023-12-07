0c66faed52359c8e625e2285dc8ffd9b2c18dc9e
Brad Meyer <wbradmeyer@gmail.com>
2023-12-07 14:44:53 -0500

    more on fund4

--- fundamentals4.js
+++ fundamentals4.js
@@ -36,3 +36,78 @@ there were 10 days with volcanos, 15 others with tsunamis, 20 earthquakes, 25 bl
 30 meteors (for 100 days total). If these probabilities continue, write whatHappensToday() 
 to print a day’s outcome. */
 
+
+
+// What Really Happened
+/* Kyle (smarter than Kenny) notes that the chance of one disaster should be unrelated 
+to the chance of another. Change whatHappensToday() function to create whatReallyHappensToday(). 
+In this new function test for each disaster independently, instead of assuming exactly one disaster 
+will happen. In other words, with this new function, all five might occur today – or none. Maybe 
+Kenny will survive! */
+
+
+
+// Soaring IQ
+/* Your time at the Dojo will definitely make you smarter! Let’s say a new Dojo student, 
+Bogdan entered with a modest IQ of 101. Let’s say that during a 14-week bootcamp, his IQ 
+rose by .01 on the first day, then went up by an additional .02 on the second day, then 
+up by .03 more on the third day, etc. all the way until increasing by .98 on his 98th day 
+(the end of 14 full weeks). What is Bogdan’s final IQ? */
+
+const soaringIq = (iq, days) => {
+    for(let i = 1; i <= days; i++){
+        iq += i/100
+    }
+    return iq
+}
+
+console.log(soaringIq(101, 98))
+
+// Letter Grade
+/* Mr. Cerise teaches high school math. Write a function that assigns and prints a letter 
+grade, given an integer representing a score from 0 to 100? Those getting 90+ get an ‘A’, 
+80-89 earn ‘B’, 70-79 is a ‘C’, 60-69 should get a ‘D’, and lower than 60 receive ‘F’. 
+For example, given 88, you should log "Score: 88. Grade: B". Given the score 61, log the 
+string "Score: 61. Grade: D". */
+
+const letterGrade = (numGrade) => {
+    let letter = ''
+    if(numGrade >= 90) letter = 'A'
+    else if(numGrade >= 80) letter = 'B'
+    else if(numGrade >= 70) letter = 'C'
+    else if(numGrade >= 60) letter = 'D'
+    else letter = 'F'
+
+    return `Score: ${numGrade}. Grade: ${letter}.`
+}
+
+console.log(letterGrade(88))
+console.log(letterGrade(76))
+
+
+// More Accurate Grades
+/* For an additional challenge, add ‘-’ signs to scores in the bottom two percent of A, B, C 
+and D scores, and “+” signs to the top two percent of B, C and D scores (sorry, Mr. Cerise 
+never gives an A+). Given 88, console.log "Score: 88. Grade: B+". Given 61, log "Score: 61. 
+Grade: D-" . */
+
+const moreAccurateGrades = (numGrade) => {
+    let letter = ''
+    if(numGrade >= 90) letter = 'A'
+    else if(numGrade >= 80) letter = 'B'
+    else if(numGrade >= 70) letter = 'C'
+    else if(numGrade >= 60) letter = 'D'
+    else letter = 'F'
+
+    let minus = [60,61,70,71,80,81,90,91]
+    let plus = [68,69,78,79,88,89]
+
+    if(minus.includes(numGrade)) letter += '-'
+    else if(plus.includes(numGrade)) letter += '+'
+
+    return `Score: ${numGrade}. Grade: ${letter}.`
+}
+
+console.log(moreAccurateGrades(88))
+console.log(moreAccurateGrades(76))
+console.log(moreAccurateGrades(91))
