80c0d9805d15e33ecd52459ab97249353510aeb8
Brad Meyer <wbradmeyer@gmail.com>
2024-01-22 12:08:00 -0500

    add other string 3 problems

--- strings2and3.js
+++ strings2and3.js
@@ -95,3 +95,21 @@ console.log(bracesValid("W(a{t}s[o(n{ c}o)m]e )h[e{r}e]!"))
 console.log(bracesValid("D(i{a}l[ t]o)n{e"))
 // needs to be reworked for this one, crossing items
 console.log(bracesValid("A(1)s[O (n]0{t) 0}k"))
+
+// Is Palindrome
+// Strings like "Able was I, ere I saw Elba" or "Madam, I'm Adam" could be considered palindromes, because 
+// (if we ignore spaces, punctuation, and capitalization) the letters are the same when reading from the back 
+// to the front. Create a function that returns a boolean whether the string is a strict palindrome. For "a x a" 
+// or "racecar", return true. Do not ignore spaces, punctuation and capitalization: if given "Dud" or "oho!", 
+// return false.
+// Second: now do ignore white space (spaces, tabs, returns), capitalization and punctuation.
+
+
+
+// Longest Palindrome
+// For this challenge, we will look not only at the entire string provided but also at the substrings within it. 
+// Return the longest palindromic substring. Given "what up, daddy-o?", return "dad". Given "uh... not much", return "u". 
+// Include spaces as well (i.e. be strict, as in previous challenge): given "Yikes! my favorite racecar erupted!", 
+// return "e racecar e". Strings longer or shorter than complete words are OK.
+// Second: re-solve the above problem, but ignore spaces, tabs, returns, capitalization and punctuation. 
+// Given "Hot puree eruption!", return "tpureeerupt".
