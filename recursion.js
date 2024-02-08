ffeb7531b4157d81b18d6871fd1115e68b39f2ab
W. Brad Meyer <wbradmeyer@gmail.com>
2024-02-08 14:59:24 -0500

    factorial

--- recursion.js
+++ recursion.js
@@ -14,8 +14,31 @@ console.log(rSigma(5))
 console.log(rSigma(2.5))
 console.log(rSigma(-1))
 
+// given answer
+function rSigma(num) {
+    console.log(num)
+    if(num > 0) {
+        return rSigma(num-1) + num;
+    }
+    return 0
+}
+
+console.log(rSigma(5))
+console.log(rSigma(2.5)) // doesn't work for decimals
 
 // Recursive Factorial
 // Given num, return the product of ints from 1 up to num. If less than zero, treat as zero. 
 // If not an integer, truncate. Experts tell us 0! is 1. rFact(3) = 6 (1*2*3). 
 // Also, rFact(6.5) = 720 (1*2*3*4*5*6).
+
+const rFact = (num, prod) =>{
+    if(!prod) prod = 1
+    if(num < 1) return prod
+    prod *= Math.floor(num)
+    num--
+    return rFact(num, prod)
+}
+
+console.log(rFact(3))
+console.log(rFact(6.5))
+console.log(rFact(-1))
