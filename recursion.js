e81a59b018284acb4d9362ca462b11f4de06aca1
W. Brad Meyer <wbradmeyer@gmail.com>
2024-02-08 22:07:04 -0500

    recursive tribonacci

--- recursion.js
+++ recursion.js
@@ -58,3 +58,21 @@ console.log(rFib(3))
 console.log(rFib(4))
 console.log(rFib(5))
 console.log(rFib(6))
+
+// Recursive Tribonacci
+// Write function rTrib(num) to mimic Fibonacci, adding previous three values instead of two. 
+// First three Tribonacci numbers are 0, 0, 1, so rTrib(3) = 1 (0+0+1); rTrib(4) = 2 (0+1+1); 
+// rTrib(5) = 4 (1+1+2); rTrib(6) = 7 (1+2+4). Handle negatives and non-integers appropriately and inexpensively.
+
+const rTrib = (num) =>{
+    if(!Number.isInteger(num)) num = Math.trunc(num)
+    else if(num == 0 || num == 1) return 0
+    else if(num == 2) return 1
+    return rTrib(num-1) + rTrib(num-2) + rTrib(num-3)
+}
+
+console.log(rTrib(3))
+console.log(rTrib(4))
+console.log(rTrib(5))
+console.log(rTrib(6))
+console.log(rTrib(6.5))
