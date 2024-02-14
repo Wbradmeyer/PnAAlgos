187351ed6e6ee7a570111a28d46ffe4bed1c977d
W. Brad Meyer <wbradmeyer@gmail.com>
2024-02-14 14:26:54 -0500

    array concat

--- arrays.js
+++ arrays.js
@@ -51,3 +51,19 @@ console.log(rotateArr([1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2
     1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,
     1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,
     1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9], 3))
+
+
+// Concat
+// Replicate JavaScript's concat(). Create a standalone function that accepts two arrays. 
+// Return a new array containing the first array's elements, followed by the second array's elements. 
+// Do not alter the original arrays. Ex.: arrConcat( ['a','b'], [1,2] ) should return new array ['a','b',1,2].
+
+const arrConcat = (arr1, arr2) =>{
+    let newArr = [...arr1]
+    for(let i=0; i < arr2.length; i++){
+        newArr.push(arr2[i])
+    }
+    return newArr
+}
+
+console.log(arrConcat(['a','b'], [1,2]))
