e6f14d274595d04b67a3e2ccdee171106f24eacf
W. Brad Meyer <wbradmeyer@gmail.com>
2024-02-15 22:25:48 -0500

    array second to last

--- arrays.js
+++ arrays.js
@@ -131,3 +131,15 @@ const removeNegatives2 = arr =>{
 }
 
 console.log(removeNegatives2([1,-1,2,-2,3,-3]))
+
+// Second-to-Last
+// Return the second-to-last element of an array. Given [42,true,4,"Kate",7], return "Kate". 
+// If array is too short, return null.
+
+const secondToLast = arr =>{
+    if(arr.length < 2) return null
+    return arr[arr.length-2]
+}
+
+console.log(secondToLast([42,true,4,"Kate",7]))
+
