d2cb581fd2f33a14cc155dd24ce11464833bc585
W. Brad Meyer <wbradmeyer@gmail.com>
2024-02-21 22:03:54 -0500

    array shuffle

--- arrays.js
+++ arrays.js
@@ -200,3 +200,20 @@ const isCreditCardValid = (arr) =>{
 }
 
 console.log(isCreditCardValid([5,2,2,8,2]))
+
+// Shuffle
+// In JavaScript, the Array object has numerous useful methods. It does not, however, contain a method that 
+// will randomize the order of an array’s elements. Let’s create shuffle(arr), to efficiently shuffle a given 
+// array’s values. Work in-place, naturally. Do you need to return anything from your function?
+
+const shuffle = (arr) =>{
+    for(let i=0; i < arr.length; i++){
+        let randIndex = Math.floor(Math.random()*arr.length)
+        let temp = arr[i]
+        arr[i] = arr[randIndex]
+        arr[randIndex] = temp
+    }
+    return arr
+}
+
+console.log(shuffle([1,2,3,4,5,6,7]))
