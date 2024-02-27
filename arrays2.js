7b78f1fe72e408e190cac2cfa7421c9d3256253c
W. Brad Meyer <wbradmeyer@gmail.com>
2024-02-27 18:27:28 -0500

    array balance index

--- arrays2.js
+++ arrays2.js
@@ -33,3 +33,30 @@ const balancePoint = (arr) =>{
 
 console.log(balancePoint([1,2,3,4,10]))
 console.log(balancePoint([1,2,4,2,1]))
+
+// Balance Index
+// Here, a balance point is on an index, not between indices. Return the balance index where sums are equal on 
+// either side (exclude its own value). Return -1 if none exist. Ex.: [-2,5,7,0,3] → 2, but [9,9] → -1.
+
+const balanceIndex = (arr) =>{
+    if(arr.length < 3) return -1
+    let sumLeft = arr[0]
+    let sumRight = 0
+    for(let i=1; i < arr.length-1; i++){
+        for(let j=i+1; j < arr.length; j++){
+            sumRight += arr[j]
+        }
+
+        if(sumLeft === sumRight) return i
+        else {
+            sumLeft += arr[i]
+            sumRight = 0
+        }
+    }
+    return -1
+}
+
+console.log(balanceIndex([-2,5,7,0,3]))
+console.log(balanceIndex([9,9]))
+console.log(balanceIndex([9,9,1,1,1,2,7]))
+console.log(balanceIndex([3,2,1,6,1,2,3]))
