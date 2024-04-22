bf8b5244f0b4617271bfd1cbf3dae759fb28fd36
W. Brad Meyer <wbradmeyer@gmail.com>
2024-04-21 22:36:07 -0400

    new array file, zoo and grid

---
+++ otherArrays.js
@@ -0,0 +1,37 @@
+// Zoo Inventory
+// Define a function, zooInventory, that accepts a multi-dimensional array of animal facts.
+// zooInventory should return a new, flat array. Each element in the new array should be a 
+// sentence about each of the animals in the zoo.
+
+let myZoo = [
+    ['King Kong', ['gorilla', 42]],
+    ['Nemo', ['fish', 5]],
+    ['Punxsutawney Phil', ['groundhog', 11]]
+];
+
+const zooInventory = (arr) => {
+    let newArr = []
+    for(let i = 0; i < arr.length; i++){
+        newArr.push(`${arr[i][0]} the ${arr[i][1][0]} is ${arr[i][1][1]}`)
+    }
+    return newArr
+}
+console.log(zooInventory(myZoo))
+
+// Make Grid
+// Write a function makeGrid that accepts two arguments: numColumns (number) + numRows (number)
+// makeGrid should return a two-dimensional array that represents a grid of the given dimensions.
+
+const makeGrid = (cols, rows) => {
+    let colArr = []
+    let gridArr = []
+    for(let i = 0; i < cols; i++){
+        colArr.push(i + 1)
+    }
+    for(let j = 0; j < rows; j++){
+        gridArr.push(colArr)
+    }
+    return gridArr
+}
+
+console.log(makeGrid(3,4))
