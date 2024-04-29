891fae2b44e0d0373efb508871b20d01e006c980
W. Brad Meyer <wbradmeyer@gmail.com>
2024-04-28 22:51:49 -0400

    add objects file

---
+++ objects.js
@@ -0,0 +1,5 @@
+// Last Friday Night
+// According to the song, Katy Perry "maxed our credit cards".
+// Define a function, lastFridayNight, takes an array of transactions.
+// lastFridayNight should return the total amount she spent last Friday night.
+
