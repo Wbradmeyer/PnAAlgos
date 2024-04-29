359b0267a32f6b8ddb628746c2677430b590c163
W. Brad Meyer <wbradmeyer@gmail.com>
2024-04-29 14:36:17 -0400

    object amounts

--- objects.js
+++ objects.js
@@ -3,3 +3,33 @@
 // Define a function, lastFridayNight, takes an array of transactions.
 // lastFridayNight should return the total amount she spent last Friday night.
 
+let transactions = [
+    {
+        name: "Tons of glitter",
+        amount: 70
+    },
+    {
+        name: "Porcelain Pink Flamingos",
+        amount: 92
+    },
+    {
+        name: "Chandelier replacement",
+        amount: 10000,
+    },
+    {
+        name: "Dinner at TGIF x6",
+        amount: 350
+    }
+];
+
+const lastFridayNight = (arr) => {
+    let total = 0
+    for(let i = 0; i < arr.length; i++){
+        total += arr[i].amount
+    }
+    return total
+}
+
+console.log(lastFridayNight(transactions))
+
+
