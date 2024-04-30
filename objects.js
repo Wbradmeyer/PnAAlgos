59499affd34552ba915b8035168be5b9a7fd2589
W. Brad Meyer <wbradmeyer@gmail.com>
2024-04-29 22:04:34 -0400

    dog breeder

--- objects.js
+++ objects.js
@@ -106,3 +106,22 @@ const freqAnalysis = (str) => {
 
 console.log(freqAnalysis('abca'))
 console.log(freqAnalysis('abracadabra yo'))
+
+
+// Dog Breeder
+// Define a function, dogBreeder, that accepts up to two optional arguments:
+// name (string), age (number)
+// dogBreeder should return an object that represents a new dog! If the user doesn't define a name, 
+// assume the dog's name is Steve. If the user doesn't define the dog's age, assume the dog's age is 0.
+
+const dogBreeder = (var1, var2) => {
+    const dog = {name: 'Steve', age: 0}
+    if(typeof var1 == 'string') dog.name = var1
+    else if(typeof var1 == 'number') dog.age = var1
+    if(var2) dog.age = var2
+    return dog
+}
+
+console.log(dogBreeder('Sam', 12))
+console.log(dogBreeder(15))
+console.log(dogBreeder())
