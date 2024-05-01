f575f62f39abbd52075a64f3af134b71a8493830
W. Brad Meyer <wbradmeyer@gmail.com>
2024-04-30 23:05:43 -0400

    outline attendance check

--- objects.js
+++ objects.js
@@ -125,3 +125,59 @@ const dogBreeder = (var1, var2) => {
 console.log(dogBreeder('Sam', 12))
 console.log(dogBreeder(15))
 console.log(dogBreeder())
+
+
+// Attendance Check
+// Define a function, attendanceCheck, that accepts a day of the week as a string.
+// Using the globally-defined classRoom array, attendanceCheck should return a new array with only the names 
+// of the students present on the inputted day of the week.
+
+let classRoom = [
+    {
+    "Marnie" : [
+            {"Monday" : true},
+            {"Tuesday" : true},
+            {"Wednesday" : true},
+            {"Thursday" : true},
+            {"Friday" : true}
+        ]
+    },
+    {
+    "Lena" : [
+            {"Monday" : false},
+            {"Tuesday" : false},
+            {"Wednesday" : true},
+            {"Thursday" : false},
+            {"Friday" : true}
+        ]
+    },
+    {
+    "Shoshanna" : [
+            {"Monday" : true},
+            {"Tuesday" : true},
+            {"Wednesday" : false},
+            {"Thursday" : true},
+            {"Friday" : false}
+        ]
+    },
+    {
+    "Jessa" : [
+            {"Monday" : false},
+            {"Tuesday" : false},
+            {"Wednesday" : false},
+            {"Thursday" : false},
+            {"Friday" : true}
+        ]
+    }
+];
+
+const attendanceCheck = (day) => {
+    const present = []
+    for(let student of classRoom){
+        present.push(classRoom[student])
+    }
+    return present
+}
+
+console.log(attendanceCheck('Monday'))
+console.log(attendanceCheck('Wednesday'))
