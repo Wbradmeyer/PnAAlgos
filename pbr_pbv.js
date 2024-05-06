1a05e448ccdcd3ab407ec9238184b7392a5df2bc
W. Brad Meyer <wbradmeyer@gmail.com>
2024-05-06 14:10:05 -0400

    clone machine

--- pbr_pbv.js
+++ pbr_pbv.js
@@ -40,3 +40,26 @@ console.log('countRemoved:', countRemoved);
 // 4
 
 
+// Clone Machine
+// Write a function cloneMachine, that, given an animal (represented as an object), returns a clone of the 
+// original animal. The name of the clone should be the name of its parent, concatenated with the word 'Clone'.
+// cloneMachine should also push the name of the clone to the parent's offspring array.
+
+const cloneMachine = (animal) => {
+    const clone = {...animal}
+    clone.name += 'Clone'
+    clone.offspring = [...animal.offspring]
+    animal.offspring.push(clone.name)
+    return clone
+}
+
+let dolly = {
+    name: 'Dolly',
+    species: 'sheep',
+    offspring: []
+}
+
+let dollyClone = cloneMachine(dolly);
+
+console.log(dollyClone) // {name: 'DollyClone', species: 'sheep', offspring: []}
+console.log(dolly) // {name: 'Dolly', species: 'sheep', offspring: ['DollyClone']}
