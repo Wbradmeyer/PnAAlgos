e3467de5efc7196281a4a8594bcee0cbc5f49900
W. Brad Meyer <wbradmeyer@gmail.com>
2024-05-14 10:39:56 -0400

    whosASpecial

--- pbr_pbv.js
+++ pbr_pbv.js
@@ -225,3 +225,34 @@ console.log(finderFunction(numbers, isEven)) // 3
 console.log(finderFunction(odds, isEven)) // -1
 
 
+// Who's a Special?
+// Write a function whosASpecial that takes an array of pets and returns a string confirming what you already knew: 
+// all of the pets are very special! Feel free to add your own special pets to the array!
+// Use .forEach in your answer.
+
+let specialPets = [{
+    name: 'Sadie',
+    species: 'cat'
+    }, {
+    name: 'Layla',
+    species: 'cat'
+    }, {
+    name: 'Bogie',
+    species: 'dog'
+    }
+];
+
+const whosASpecial = (arr) => {
+    let resultStr = []
+    arr.forEach(element => {
+        resultStr.push(`${element.name} the ${element.species} is very special!`)
+    });
+    resultStr = resultStr.join(' ')
+    console.log(resultStr)
+    return resultStr
+}
+
+whosASpecial(specialPets);
+// => Sadie the cat is very special! Layla the cat is very special! Bogie the dog is very special!
+
+
