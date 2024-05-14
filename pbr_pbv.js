4cfd65fdf1eba94d4650c8530ed7f48d440fc1bb
W. Brad Meyer <wbradmeyer@gmail.com>
2024-05-14 10:10:12 -0400

    finder function

--- pbr_pbv.js
+++ pbr_pbv.js
@@ -202,3 +202,26 @@ let squareOfFour = functionLogger(squareNum, 4);
 console.log(squareOfFour)    // 16
 
 
+// Finder Function
+// Define a function 'finderFunction' that takes an array and a callback. Pass each element from the array into the 
+// callback. If the callback returns true, return the index of the current element. If the callback never returns true, 
+// return -1;
+
+let numbers = [1, 3, 5, 64, 7, 12];
+let odds = [9, 13, 15, 17];
+
+function isEven(num) {
+    return !(num % 2);
+}
+
+const finderFunction = (arr, func) => {
+    for(i = 0; i < arr.length; i++){
+        if(func(arr[i])) return i
+    }
+    return -1
+}
+
+console.log(finderFunction(numbers, isEven)) // 3
+console.log(finderFunction(odds, isEven)) // -1
+
+
