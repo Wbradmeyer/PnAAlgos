bc28733d36e10a824b75f6ac873b023b0a43257e
W. Brad Meyer <wbradmeyer@gmail.com>
2024-05-16 11:44:09 -0400

    partial

--- pbr_pbv.js
+++ pbr_pbv.js
@@ -352,3 +352,21 @@ let twelvesTable = timesTable(12);
 console.log(twelvesTable(100))    // => 1200
 
 
+// Partial
+// Write a function, partial, that accepts a callback and one argument (let's call it 'arg a'). Partial should return 
+// a new function. When invoked, the new function will:
+// Accept its own argument (let's call it 'arg b')
+// Call the callback, passing in 'arg a' as the first argument and 'arg b' as the second argument
+// Return the value returned by the callback
+
+function summer (a, b) {
+    return a + b
+};
+
+const partial = (callback, argA) => {
+    const newFunc = (argB) => callback(argA, argB)
+    return newFunc
+}
+
+let sumFive = partial(summer, 5);
+console.log(sumFive(10)) // => 15;
